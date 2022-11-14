10bfc0059c3c64379565eea3fb814877409ad8f2
Rajayne <ryann.j.nealey@gmail.com>
2022-11-14 13:58:36 -0500

    create eventListener to flip and match cards

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const gameContainer = document.getElementById("game");
+let card1 = null;
+let card2 = null;
+let cardsFlipped = 0;
+let noClicking = false;
 
 const COLORS = [
   "red",
@@ -45,22 +49,58 @@ function createDivsForColors(colorArray) {
   for (let color of colorArray) {
     // create a new div
     const newDiv = document.createElement("div");
-
     // give it a class attribute for the value we are looping over
     newDiv.classList.add(color);
-
     // call a function handleCardClick when a div is clicked on
     newDiv.addEventListener("click", handleCardClick);
-
     // append the div to the element with an id of game
     gameContainer.append(newDiv);
   }
 }
 
 // TODO: Implement this function!
-function handleCardClick(event) {
+function handleCardClick(e) {
+  if (noClicking) return;
+  if (e.target.classList.contains("flipped")) return;
+
+  let currentCard = e.target;
+  currentCard.style.backgroundColor = currentCard.classList[0];
+
+  if (!card1 || !card2) {
+    currentCard.classList.add("flipped");
+    card1 = card1 || currentCard;
+    card2 = currentCard === card1 ? null : currentCard;
+  }
+
+  if (card1 && card2) {
+    noClicking = true;
+  
+  let gif1 = card1.className;
+  let gif2 = card2.className;
+
+  if (gif1 === gif2) {
+    cardsFlipped += 2;
+    card1.removeEventListener("click", handleCardClick);
+    card2.removeEventListener("click", handleCardClick);
+    card1 = null;
+    card2 = null;
+    noClicking = false;
+  } else {
+      setTimeout(function() {
+        card1.style.backgroundColor = "";
+        card2.style.backgroundColor = "";
+        card1.classList.remove('flipped');
+        card2.classList.remove('flipped');
+        card1 = null;
+        card2 = null;
+        noClicking = false;
+      }, 1000);
+    }
+  }
+
+  if (cardsFlipped === COLORS.length) alert("game over!")
   // you can use event.target to see which element was clicked
-  console.log("you just clicked", event.target);
+  console.log("you just clicked", e.target);
 }
 
 // when the DOM loads
